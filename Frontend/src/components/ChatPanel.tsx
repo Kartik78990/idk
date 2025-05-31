93c88a7ed434f9c78b7e4161b561b6d904904a4b
Kartik Pundir <119773936+Kartik78990@users.noreply.github.com>
2025-05-31 08:06:01 +0530

    Update ChatPanel.tsx

--- Frontend/src/components/ChatPanel.tsx
+++ Frontend/src/components/ChatPanel.tsx
@@ -43,7 +43,7 @@ const ChatPanel: React.FC<ChatPanelProps> = ({ onBackClick }) => {
   }, [messages, typingMessage]);
 
   useEffect(() => {
-    const ws = new WebSocket('${import.meta.env.VITE_WS_URL}/ws');
+    const ws = new WebSocket('wss://idk-1-5xzk.onrender.com/ws');
 
     ws.onopen = () => console.log("✅ WebSocket connected");
 
